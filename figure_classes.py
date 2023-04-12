5d6bba315f7d30ba4f49bc46b2c61ee8f2704385
sunseek3r <jaraxxeu@gmail.com>
2023-04-12 15:30:33 +0300

    code refactioring + gitignore

--- figure_classes.py
+++ figure_classes.py
@@ -1,3 +1,20 @@
 """
     A file for figure classes (look TODO in main.py)
 """
+
+
+class Line():
+    """
+        This is a class for Line figure
+    """
+
+    def __init__(self, d1, d2) -> None:
+        self.d1 = d1
+        self.d2 = d2
+
+    def draw(self, ax):
+        x_values = [self.d1[0], self.d2[0]]
+        y_values = [self.d1[1], self.d2[1]]
+        z_values = [self.d1[2], self.d2[2]]
+
+        ax.plot(x_values, y_values, z_values, 'bo', linestyle='-')
--- main.py
+++ main.py
@@ -5,10 +5,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from figure_classes import *
+
 
 # Function to create and display the 3D plot
 
-plots = []
 
 """
 TODO:
@@ -28,59 +29,63 @@ TODO:
 
 """
 
-def simple_function():
-    """
-      This function doesn't do notging
-      ------
-      Inputs:
-      
-      -------
-      Returns:
-    """
-    
 
-def draw_3d_plot():
+
+
+def draw_plots(plots, ax):
+    """
+        This function draws all plots
+    """
+    for figure in plots:
+        figure.draw(ax)
+
+    return ax
+
+def main():
+    """
+    Main launch code
+    """
+
+    plots = [Line([0, 1, 2], [2, 3, 5])]    
+
     fig = plt.figure()
     ax = fig.add_subplot(111, projection="3d")
-
-    point1 = [1, 3, 2]
-    point2 = [-1, 4, -8]
-
-    x_values = [point1[0], point2[0]]
-    y_values = [point1[1], point2[1]]
-    z_values = [point1[2], point2[2]]
-
-    ax.plot(x_values, y_values, z_values, 'bo', linestyle='-')
 
     ax.set_xlabel("X Axis")
     ax.set_ylabel("Y Axis")
     ax.set_zlabel("Z Axis")
 
+    # Create main window
+    root = tk.Tk()
+    root.title("Interactive 3D Plot")
+
+    # Create main frame
+    main_frame = ttk.Frame(root)
+    main_frame.grid(row=0, column=0, padx=5, pady=5)
+
+    # Create plot frame
+    frame_plot = ttk.Frame(main_frame)
+    frame_plot.grid(row=0, column=0)
+
+    # Create control frame
+    frame_control = ttk.Frame(main_frame)
+    frame_control.grid(row=0, column=1, padx=5, pady=5)
+
+    def draw_button_pressed():
+        global ax
+        ax = draw_plots(plots, ax)
+
+    # Add button to draw plot
+    btn_draw = ttk.Button(frame_control, text="Draw 3D Plot",
+                          command=draw_button_pressed)
+    btn_draw.grid(row=0, column=0, pady=5)
+
     canvas = FigureCanvasTkAgg(fig, master=frame_plot)
     canvas.draw()
     canvas.get_tk_widget().grid(row=0, column=0)
 
-# Create main window
-root = tk.Tk()
-root.title("Interactive 3D Plot")
-
-# Create main frame
-main_frame = ttk.Frame(root)
-main_frame.grid(row=0, column=0, padx=5, pady=5)
-
-# Create plot frame
-frame_plot = ttk.Frame(main_frame)
-frame_plot.grid(row=0, column=0)
-
-# Create control frame
-frame_control = ttk.Frame(main_frame)
-frame_control.grid(row=0, column=1, padx=5, pady=5)
-
-# Add button to draw plot
-btn_draw = ttk.Button(frame_control, text="Draw 3D Plot", command=draw_3d_plot)
-btn_draw.grid(row=0, column=0, pady=5)
-
-root.mainloop()
+    root.mainloop()
 
 
-
+if __name__ == '__main__':
+    main()
