ded27fce75d31211e42938d6c74ae0682e7291f1
sunseek3r <jaraxxeu@gmail.com>
2023-04-27 20:26:58 +0300

    Surface added

--- inputs.py
+++ inputs.py
@@ -4,15 +4,17 @@ class SphereDialog(QDialog):
     def __init__(self, parent=None):
         super().__init__(parent)
 
+        self.setWindowTitle("Input centre of sphere")
+
         self.x1 = QLineEdit(self)
         self.y1 = QLineEdit(self)
         self.z1 = QLineEdit(self)
-        buttonBox = QDialogButtonBox(QDialogButtonBox.Ok | QDialogButtonBox.Cancel, self);
+        buttonBox = QDialogButtonBox(QDialogButtonBox.Ok | QDialogButtonBox.Cancel, self)
 
         layout = QFormLayout(self)
-        layout.addRow("X1", self.x1)
-        layout.addRow("Y1", self.y1)
-        layout.addRow("Z1", self.z1)
+        layout.addRow("X", self.x1)
+        layout.addRow("Y", self.y1)
+        layout.addRow("Z", self.z1)
         layout.addWidget(buttonBox)
 
         buttonBox.accepted.connect(self.accept)
@@ -20,25 +22,25 @@ class SphereDialog(QDialog):
 
     def getInputs(self):
         return (self.x1.text(), self.y1.text(), self.z1.text())
-    
 
-class LineDialog(QDialog):
-    def __init__(self, number, parent=None):
+class PointDialog(QDialog):
+    def __init__(self, title, parent=None):
         super().__init__(parent)
 
+        self.setWindowTitle(title)
         self.x1 = QLineEdit(self)
         self.y1 = QLineEdit(self)
         self.z1 = QLineEdit(self)
         buttonBox = QDialogButtonBox(QDialogButtonBox.Ok | QDialogButtonBox.Cancel, self);
 
         layout = QFormLayout(self)
-        layout.addRow(f"X{number}", self.x1)
-        layout.addRow(f"Y{number}", self.y1)
-        layout.addRow(f"Z{number}", self.z1)
+        layout.addRow("X", self.x1)
+        layout.addRow("Y", self.y1)
+        layout.addRow("Z", self.z1)
         layout.addWidget(buttonBox)
 
         buttonBox.accepted.connect(self.accept)
         buttonBox.rejected.connect(self.reject)
 
     def getInputs(self):
-        return (self.x1.text(), self.y1.text(), self.z1.text())
+        return [float(i) for i in (self.x1.text(), self.y1.text(), self.z1.text())]
--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from pyvistaqt import QtInteractor, MainWindow, BackgroundPlotter
 import pyvista as pv
 import numpy as np
 from figure_classes import *
-from inputs import SphereDialog, LineDialog
+from inputs import SphereDialog, PointDialog
 from settings import Settings
 
 # Function to create and display the 3D plot
@@ -73,6 +73,10 @@ class Window(MainWindow):
         line_button = QPushButton("Line", self)
         line_button.clicked.connect(self.add_line)
         left_layout.addWidget(line_button)
+
+        surface_button = QPushButton("Surface", self)
+        surface_button.clicked.connect(self.add_surface)
+        left_layout.addWidget(surface_button)
         
         self.plotter.show_grid()
 
@@ -83,16 +87,17 @@ class Window(MainWindow):
     
 
     def add_line(self):
-        dialog = LineDialog(1)
+        dialog = PointDialog("Input point 1")
         point1 = []
         if dialog.exec():
             point1 = dialog.getInputs()
         
-        dialog = LineDialog(2)
+        dialog = PointDialog("Input point 2")
         point2 = []
         if dialog.exec():
             point2 = dialog.getInputs()
-
+        if len(point1) != 3 or len(point2) != 3:
+            return
         point1 = [float(i) for i in point1]
         point2 = [float(i) for i in point2]
 
@@ -106,18 +111,59 @@ class Window(MainWindow):
         centre = []
         if dialog.exec():
             centre = dialog.getInputs()
+        if centre is None or len(centre) != 3:
+            return
         centre = [float(i) for i in centre]
         radius, ok = QInputDialog.getDouble(self, "Input", "Radius")
-        if ok:
+        print(radius)
+        if ok and radius != 0:
             sphere = pv.Sphere(radius, centre)
             self.plotter.add_mesh(sphere, opacity=0.5, show_edges=False)
             self.text_box.append(f"Sphere: centre:{centre}, radius:{radius}")
             self.plotter.reset_camera()
 
-        self.settings.update_bounds(sphere.bounds)
+            self.settings.update_bounds(sphere.bounds)
 
     def add_surface(self):
-        pass
+        dialog = PointDialog("Input point C")
+
+        point_c = []
+
+        if dialog.exec():
+            point_c = dialog.getInputs()
+        
+        dialog = PointDialog("Input vector N")
+
+        vector_n = []
+
+        if dialog.exec():
+            vector_n = dialog.getInputs()
+
+        if len(point_c) != 3 or len(vector_n) != 3:
+            return
+        
+
+        A = vector_n[0]
+        B = vector_n[1]
+        C = vector_n[2]
+        D = -sum([i*j for i,j in zip(point_c, vector_n)])
+
+        x = np.arange(self.settings.x_bounds[0], self.settings.x_bounds[1], 0.1)
+        y = np.arange(self.settings.y_bounds[0], self.settings.y_bounds[1], 0.1)
+
+        x, y = np.meshgrid(x, y)
+
+        x0 = point_c[0]
+        y0 = point_c[1]
+        z0 = point_c[2]
+
+        z = np.array([A*i + B * j + D for i,j in zip(x, y)])
+        z = z / C
+
+        grid = pv.StructuredGrid(x, y, z)
+        self.plotter.add_mesh(grid, opacity=0.7)
+        self.text_box.append("surface")
+        self.plotter.reset_camera()
         
 
 if __name__ == '__main__':
